38a8eb6c70b75954e44dee34ba673ee9a3dedcf9
parisdolfman <parisdolfman@gmail.com>
2021-02-28 15:14:02 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,32 @@
 class TweetsController < ApplicationController
+    get '/tweets' do
+        if logged_in?
+          @tweets = Tweet.all
+          erb :'tweets/tweet_list'
+        else
+          redirect to '/login'
+        end
+      end
 
+      get '/tweets/new' do
+        if logged_in?
+          erb :'tweets/new'
+        else
+          redirect to '/login'
+        end
+      end
 
 end
+
+get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      if @tweet && @tweet.user == current_user
+        erb :'tweets/edit_'
+      else
+        redirect to '/tweet_list'
+      end
+    else
+      redirect to '/login'
+    end
+  end
