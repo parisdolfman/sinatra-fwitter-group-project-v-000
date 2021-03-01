e6e1d15e85ef64e2c634e443a782c68cdffbf7a1
parisdolfman <parisdolfman@gmail.com>
2021-03-01 10:44:58 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,58 +1,24 @@
 class TweetsController < ApplicationController
-    get '/tweets' do
-        if logged_in?
-          @tweets = Tweet.all
-          erb :'tweets/tweets'
-        else
-          redirect to '/login'
-        end
-      end
 
-      get '/tweets/new' do
-        if logged_in?
-          erb :'tweets/new'
-        else
-          redirect to '/login'
-        end
-      end
-
+get '/tweets' do
+  if logged_in?
+    @tweets = Tweet.all
+    erb :'tweets/tweets'
+  else
+    redirect to '/login'
+  end
+end
+    
+get '/tweets/new' do
+  if logged_in?
+    erb :'tweets/new'
+  else
+    redirect to '/login'
+  end
 end
 
-#NOTE: Below does not solve any errors
-# get '/tweets/:id' do
-#     case 
-#     when logged_in? == true 
-#       @tweet = Tweet.find_by_id(params[:id])
-#     when @tweet && @tweet.user == current_user
-#         erb :'tweets/edit_tweet'
-#     when @tweet.update(content: params[:content])
-#         redirect to "/tweets/#{@tweet.id}"
-#     when !!@tweet.update(content: params[:content])
-#         redirect to "/tweets/#{@tweet.id}/edit"
-#     when @tweet && !!@tweet.user == current_user
-#         redirect to '/tweets'
-#     else
-#         redirect to '/login'
-#       end
-#   end
 
-#NNOTE: Below is easiest written way by separation of responsibility. Looking to merge the get and post method.
-
-#  get '/tweets/:id/edit' do
-#    if logged_in?
-#       if @tweet && @tweet.user == current_user
-#       @tweet = Tweet.find_by_id(params[:id])
-#       if @tweet && @tweet.user == current_user
-#         erb :'tweets/edit_tweet'
-#       else
-#         redirect to '/tweets'
-#       end
-#     else
-#       redirect to '/login'
-#     end
-#   end
-
-  post '/tweets' do
+post '/tweets' do
     if logged_in?
       if params[:content] == ""
         redirect to "/tweets/new"
@@ -69,3 +35,63 @@ end
     end
   end
 
+
+
+get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      if @tweet && @tweet.user == current_user
+        erb :'tweets/edit_tweet'
+      else
+        redirect to '/tweets'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/tweets/#{params[:id]}/edit"
+      else
+        @tweet = Tweet.find_by_id(params[:id])
+        if @tweet && @tweet.user == current_user
+          if @tweet.update(content: params[:content])
+            redirect to "/tweets/#{@tweet.id}"
+          else
+            redirect to "/tweets/#{@tweet.id}/edit"
+          end
+        else
+          redirect to '/tweets'
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      if @tweet && @tweet.user == current_user
+        @tweet.delete
+      end
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+  
+end
